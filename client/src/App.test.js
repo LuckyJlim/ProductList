11110e8f3ac484055b30e108182a09d533c37022
Junglim Lee <jlim77@gmail.com>
2021-10-18 15:41:41 +1100

    Add test script, Add env values

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import * as reactRedux from 'react-redux';
 import App from './App';
 
 test('renders learn react link', () => {
+  const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+  const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
+  useSelectorMock.mockReturnValue({
+    curPage: 1,
+    itemsPerPage: 8,
+    totalItems: 10,
+  });
+  const dummyDispatch = jest.fn();
+  useDispatchMock.mockReturnValue(dummyDispatch);
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/loading/i);
   expect(linkElement).toBeInTheDocument();
 });
